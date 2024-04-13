80aa88dfc1a7bddc4db96334d133477c6b7f46c6
Mayank Sharma <mayankofficial167@gmail.com>
2024-04-13 13:01:12 +0530

    done

--- javascript/practice/app.js
+++ javascript/practice/app.js
@@ -1,5 +1,45 @@
-let a = [1,2,5,8,3];
+h1 = document.querySelector('h1');
 
-a.forEach(function  (item) {
-    console.log("Item: " + item);
+
+// function changeColor(color,delay,nextColorChange) {
+//     setTimeout(()=>{
+        
+//     h1.style.color= color;  //change the text color of 'h1' to the parameter passed in
+//     if(nextColorChange) nextColorChange();
+//     },delay);
+// }
+
+// changeColor("red",2000,()=>{
+//     changeColor("green",2000,()=>{
+//         changeColor("yellow",2000,()=>{
+//             changeColor("Orange",2000,()=>{
+//                 changeColor("blue",2000);
+//             });
+//         });
+//     });
+// });  
+
+
+function changeColor(color,delay) {
+    return new Promise((resolve, reject)=> {
+    setTimeout(()=>{    
+    h1.style.color= color;  //change the text color of 'h1' to the parameter passed in
+    resolve("color changed");
+    },delay)
 });
+}
+// changeColor("red", 1000)
+// .then(() => {
+//     changeColor("green", 1000);
+//     return changeColor( "yellow", 1000 );
+// })
+// .then(
+//     () => {
+//         changeColor("pink", 1000)
+// })
+
+
+async function demo(){
+    await changeColor( "red", 3000 )
+    await changeColor("green", 1000)    
+}
